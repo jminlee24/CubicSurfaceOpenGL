00b67ab5aced2091d8b1e06c3a09696a588b1cab
Joshua Lee <jminlee24@outlook.com>
2024-08-06 18:13:34 -0400

    init setup

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import glm
 import moderngl as mgl
 import pygame as pg
 
+from settings import *
+from shader_program import ShaderProgram
+from scene import Scene
+
 
 class GraphicsEngine:
 
@@ -16,7 +20,7 @@ class GraphicsEngine:
     pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
     pg.display.gl_set_attribute(pg.GL_DEPTH_SIZE, 24)
 
-    pg.display.set_mode((800, 800), flags=pg.OPENGL | pg.DOUBLEBUF, vsync=False)
+    pg.display.set_mode(WIN_RES, flags=pg.OPENGL | pg.DOUBLEBUF)
     self.ctx = mgl.create_context()
 
     self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
@@ -28,19 +32,32 @@ class GraphicsEngine:
 
     self.is_running = True
 
+    self.on_init()
+
+  def on_init(self):
+    self.shader_program = ShaderProgram(self)
+    self.scene = Scene(self)
+
   def update(self):
+    self.shader_program.update()
+    self.scene.update()
+
     self.delta_time = self.clock.tick()
     self.time = pg.time.get_ticks()
 
     pg.display.set_caption(f'{self.clock.get_fps() : .0f}')
 
   def render(self):
-    self.ctx.clear()
+    self.ctx.clear(color=BG_COLOR)
+    self.scene.render()
     pg.display.flip()
 
   def handle_events(self):
     for event in pg.event.get():
-      if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE) or (event.type == pg.KEYDOWN and event.key == pg.K_q):
+      if event.type == pg.KEYDOWN:
+        if event.key == pg.K_ESCAPE or event.key == pg.K_q:
+          self.is_running = False
+      elif event.type == pg.QUIT:
         self.is_runnning = False
 
   def run(self):
---
+++ settings.py
@@ -0,0 +1,7 @@
+import numpy as np
+import math
+import glm
+
+WIN_RES = glm.vec2(1600, 800)
+
+BG_COLOR = glm.vec3(0, 0, .1)
